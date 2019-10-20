b868bca943d13a1f6858dd222d5f58588433faf9
Lawrence Anstey <lawrence.anstey@4mat.com>
2019-10-20 10:39:08 +0100

    reading the spec

---
+++ FWA/Controllers/MovieController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FWA.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MovieController : ControllerBase
+    {
+        public async Task<IActionResult> Search()
+        {
+            return null;
+        }
+
+    }
+}
---
+++ FWA/Models/SearchModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FWA.WebApi.Models
+{
+    public class SearchModel
+    {
+        public int? YearOfRelease { get; set; }
+        //public string? Title { get; set; }
+
+    }
+}
